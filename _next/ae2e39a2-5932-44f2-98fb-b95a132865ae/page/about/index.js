4bd48b27252a5221b383b221735d224471cf53f2
BankDev <keerati.suk@gmail.com>
2017-08-02 13:23:21 +0700

    Updates

---
+++ _next/ae2e39a2-5932-44f2-98fb-b95a132865ae/page/about/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([5],{339:function(e,t,u){e.exports=u(340)},340:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=u(7),r=function(e){return e&&e.__esModule?e:{default:e}}(n);t.default=function(){return r.default.createElement("div",null,r.default.createElement("img",{src:"../static/images/image.jpeg"}))}}},[339]);
+            return { page: comp.default }
+          })
+        
