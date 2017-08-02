efab13bdbd673f99a03088d4b035b7bd5336895a
BankDev <keerati.suk@gmail.com>
2017-08-03 02:44:57 +0700

    Updates

--- _next/a5ae286c-7939-47a9-b4a2-96d706411630/page/about/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([5],{340:function(e,t,a){e.exports=a(341)},341:function(e,t,a){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var u=a(7),n=l(u),d=a(113),r=l(d),f=a(114),c=l(f);t.default=function(){return n.default.createElement("div",null,n.default.createElement("h1",null,"About Page"),n.default.createElement("img",{src:r.default.BASE_URL+"/static/images/image.jpeg"}),n.default.createElement("div",null,n.default.createElement(c.default,{href:"/"},n.default.createElement("a",null,"Back To Home Page"))),n.default.createElement("div",null,n.default.createElement(c.default,{as:r.default.BASE_URL+"/a/learn-nextjs"},n.default.createElement("a",{title:r.default.BASE_URL+"/a/learn-nextjs"},"Sample PathMap ('"+r.default.BASE_URL+"/a/learn-nextjs')"))))}}},[340]);
-            return { page: comp.default }
-          })
-        
