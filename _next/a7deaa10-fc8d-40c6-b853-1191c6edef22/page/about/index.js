0e01c58f5eaad138493612def17aead83a4b6a60
BankDev <keerati.suk@gmail.com>
2017-08-03 15:18:32 +0700

    Updates

--- _next/a7deaa10-fc8d-40c6-b853-1191c6edef22/page/about/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([5],{340:function(e,t,a){e.exports=a(341)},341:function(e,t,a){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var u=a(7),n=l(u),d=a(71),f=l(d),r=a(114),c=l(r);t.default=function(){return n.default.createElement("div",null,n.default.createElement("h1",null,n.default.createElement(c.default,{as:f.default.BASE_URL+"/about"},n.default.createElement("a",null,"About Page"))),n.default.createElement("img",{src:f.default.BASE_URL+"/static/images/image.jpeg"}),n.default.createElement("div",null,n.default.createElement(c.default,{href:""+f.default.BASE_URL},n.default.createElement("a",null,"Back To Home Page"))),n.default.createElement("div",null,n.default.createElement(c.default,{as:f.default.BASE_URL+"/a/learn-nextjs"},n.default.createElement("a",{title:f.default.BASE_URL+"/a/learn-nextjs"},"Sample PathMap ('"+f.default.BASE_URL+"/a/learn-nextjs')"))))}}},[340]);
-            return { page: comp.default }
-          })
-        
